1c39483af658915554106631e39e1a90461ca89e
Nathan4Andrade <nathanandrade20@gmail.com>
2023-07-26 13:25:32 -0300

    createCostumer controller

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -12,14 +12,27 @@ export async function getCustomers(req, res) {
 export async function getCustomerById(req, res) {
   const { id } = req.params;
   try {
-    const receita = await db.query(`SELECT * FROM customers WHERE id=${id};`);
-    res.send(receita.rows[0]);
+    const customer = await db.query(`SELECT * FROM customers WHERE id=$1;`, [
+      id,
+    ]);
+    if (!customer.rows[0]) res.sendStatus(404);
+    res.send(customer.rows[0]);
   } catch (err) {
     res.status(500).send(err.message);
   }
 }
 export async function createCustomer(req, res) {
-  res.send("createCustomer");
+  const { name, phone, cpf, birthday } = req.body;
+  try {
+    const customer = await db.query(
+      `INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4);`,
+      [name, phone, cpf, birthday]
+    );
+
+    res.sendStatus(201);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 }
 
 export async function editCustomer(req, res) {
