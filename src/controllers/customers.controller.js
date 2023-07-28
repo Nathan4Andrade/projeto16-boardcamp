57de8c8fceb551c99cbda31d7c60d10a5edbb361
Nathan4Andrade <nathanandrade20@gmail.com>
2023-07-28 16:52:16 -0300

    fix: api funciona no localhost

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -35,7 +35,7 @@ export async function createCustomer(req, res) {
       `INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4);`,
       [name, phone, cpf, birthday]
     );
-    
+
     res.sendStatus(201);
   } catch (err) {
     res.status(500).send(err.message);
